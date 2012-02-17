f68c0a2b6efe26c370cc369d3f285701803fde2f
Jim Holmström <jim.holmstroem@gmail.com>
2012-02-17 18:31:55 +0100

    Added check for filetype .pngvin in video

--- src/video.py
+++ src/video.py
@@ -8,11 +8,15 @@ class video:
 
     num_digits=5
     is_frame=re.compile('^frame-[0-9]{'+str(num_digits)+'}.png$')
+    is_valid_pngvin=re.compile('.pngvin$')
 
     def __init__(self,url_to_folder):
-        for img_path in os.listdir(url_to_folder): #NOTE possible bug the order of listing the frames might change.
-            if self.is_frame.match(img_path):
-                self.vid.append(Image.open(url_to_folder+'/'+img_path)) #NOTE not crossplatform
+        if self.is_valid_pngvin.match(url_to_folder)
+            for img_path in os.listdir(url_to_folder): #NOTE possible bug the order of listing the frames might change.
+                if self.is_frame.match(img_path):
+                    self.vid.append(Image.open(url_to_folder+'/'+img_path)) #NOTE not crossplatform
+        else
+            raise Exception('Not a .pngvin file')
 
     def __getitem__(self,i):
         return self.vid[i]
