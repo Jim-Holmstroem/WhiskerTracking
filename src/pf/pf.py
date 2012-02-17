25e943248b02d3ba3bfb5bd6af253e702401caf4
Emil Lundberg <emlun@kth.se>
2012-02-17 15:01:25 +0100

    Core Particle Filter functionality

---
+++ src/pf/pf.py
@@ -0,0 +1,44 @@
+from random import random
+from numpy import zeros, zeros_like
+
+def default_sampler(X_prev):
+    raise NotImplementedError()
+
+def naive_resample(X, weights):
+    raise NotImplementedError()
+
+def low_variance_resample(X, weights):
+    X_bar = zeros_like(X)
+
+    M = len(X)
+    M_inv = 1.0/M
+    r = (M_inv)*random()
+    c = weights[0]
+
+    j = 0
+    i = 0
+    
+    for m in xrange(M):
+        u = r + m * M_inv
+        while u > c:
+            i += 1
+            c += weights[i]
+        
+        X_bar[j] = X[i]
+        j += 1
+    
+    return X_bar
+
+'''
+pf
+@param X_prev
+'''
+def pf(X_prev, observation, importance_function, sampling_function=default_sampler, resampling_function=low_variance_resample):
+    X_bar = zeros_like(X_prev)
+    weights = zeros(len(X_prev))
+
+    for rownum in xrange(len(X_prev)):
+        X_bar[rownum] = sampling_function(X_prev[rownum]) # TODO
+        weights[rownum] = importance_function(observation, X_prev[rownum]) # TODO
+    
+    return resampling_function(X_bar, weights)
