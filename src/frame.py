386881973ac8c82eefd1a9cf16c9022fe57b5385
Jim Holmström <jim.holmstroem@gmail.com>
2012-02-18 13:18:44 +0100

    Added som doc fixed a bug and refactored

--- src/frame.py
+++ src/frame.py
@@ -1,40 +1,61 @@
 class frame:
     """
-    Dual for image as numpy.array and PIL.Image with lazy loading / constructing
+    Dual for image as numpy.array and PIL.Image with lazy loading/constructing
+
+    @Example
+        f=frame(Image.load('test.png'))
+        frame.get_image().show() #no converting
+        read_array(frame.get_array()) #convert img->arr internally since img updated (from None)
+        frame.get_image().show() #no converting since arr hash haven't changed
+        write_array(frame.get_array()) #no conversion
+        frame.get_image().show() #convert arr->img since arr has updated
+
+    @NOTE AVOID (Example)
+        arr=f.get_arr()
+        img=f.get_image()
+        write2(arr)
+        img.show() #will show the old image
 
     @Note the dual could be generalized and perhaps used elsewhere
     """
 
-    img=None
-    img_hash=hash(img) #last hash in which we updated the image with
-    arr=None
-    arr_hash=hash(arr) #last hash in which we updated the array with
+    __img=None
+    __img_hash=hash(__img) #last hash in which we updated the image with
+    __arr=None
+    __arr_hash=hash(__arr) #last hash in which we updated the array with
 
     def __init__(self,image):
         raise Exception("Not tested yet")
-        self.img=image
+        self.__img=image
 
-    def update_image(self):
-        self.img=Image.fromarray(numpy.uint(self.arr))
-        self.arr_hash=hash(self.arr)
+    def __update_hash(self):
+        """
+        Called when __img and __arr is synced
+        """
+        self.__img_hash=hash(self.__img)
+        self.__arr_hash=hash(self.__arr)
 
-    def update_array(self):
-        self.arr=numpy.asarray(self.img)
-        self.img_hash=hash(self.img)
+    def __update_image(self):
+        self.__img=Image.fromarray(numpy.uint(self.__arr))
+        self.__update_hash()
+
+    def __update_array(self):
+        self.__arr=numpy.asarray(self.__img)
+        self.__update_hash()
 
     def get_image(self):
         """
 
         """
-        if(self.arr_hash!=hash(self.arr)):
-            self.update_image()
-        return self.img
+        if(self.__arr_hash!=hash(self.__arr)): #if __arr has changed
+            self.__update_image()
+        return self.__img
 
     def get_array(self):
         """
 
         """
-        if(self.img_hash!=hash(self.img)):
-            self.update_array()
-        return self.arr
+        if(self.__img_hash!=hash(self.__img)): #if __img has changed
+            self.__update_array()
+        return self.__arr
 
