6beea28a58d70de2ad10f27f53c72d57e71729a1
Jim Holmström <jim.holmstroem@gmail.com>
2012-02-19 16:27:00 +0100

    Working some more on distribution and added sample_set

--- src/wmath/distribution.py
+++ src/wmath/distribution.py
@@ -7,6 +7,7 @@ class distribution:
 
     """
 
+    __p=None
     __cumsum=None
 
     def __init__(self,p):
@@ -15,11 +16,21 @@ class distribution:
 
         @Param p numpy-array representing the probability function
         """
-        __cumsum=p.cumsum()
+        assert('cumsum' in dir(p))
+        assert('searchsorted' in dir(p))
+        self.__p=p
 
-    def sample(self,n=1):
+    def sample(self,n=1,sample_set=None):
         """
+        Get n samples from the distribution
 
         @Param n the number of samples
+        @Return vector of samples
         """
-        return [self.__cumsum.searchsorted(random()) for i in range(n)]
+        if self.__cumsum is None:
+            self.__cumsum=self.__p.cumsum()
+        indices = [self.__cumsum.searchsorted(random()) for i in range(n)]
+        if sample_set is None:
+            return indices
+        else:
+            return map(lambda i:sample_set[i],indices)
