dac5e273724af28f2c65be1ce232c0f8eb666dc8
Emil Lundberg <emlun@kth.se>
2012-03-02 17:27:20 +0100

    Made StateTransitionDatabase manage only one table instead of two, added some functions to it

--- src/wdb/wdb.py
+++ src/wdb/wdb.py
@@ -1,24 +1,26 @@
 import os
 import sqlite3
+import numpy
 
 def create_database(database_path = "data/transition-db/wdb.db", number_of_parameters = 2):
     con = sqlite3.connect(database_path)
     
     cur = con.cursor()
     
-    createStateTableQuery = "CREATE TABLE states(id INTEGER PRIMARY KEY"
-    for i in range(number_of_parameters):
-        createStateTableQuery += ", theta"+str(i)+" REAL NOT NULL"
-    createStateTableQuery += ");"
+    cur.execute("CREATE TABLE metadata(number_of_parameters INTEGER NOT NULL);")
+    cur.execute("INSERT INTO metadata VALUES(?);", [number_of_parameters])
     
-    cur.execute("PRAGMA foreign_keys = ON;");
-    cur.execute(createStateTableQuery);
-    cur.execute("CREATE TABLE transitions(" +
-                "id INTEGER PRIMARY KEY, " +
-                "fromState INTEGER NOT NULL, " +
-                "toState INTEGER NOT NULL, " +
-                "FOREIGN KEY(fromState) REFERENCES states(id), " +
-                "FOREIGN KEY(toState) REFERENCES states(id));")
+    parameter_name = "theta_"
+    column_type = " FLOAT NOT NULL"
+    createTransitionsTableQuery = "CREATE TABLE transitions("
+    
+    for prefix in ("from_", "to_"):
+        for i in xrange(number_of_parameters):
+            createTransitionsTableQuery += (prefix + parameter_name + str(i) + column_type + ", ")
+    
+    createTransitionsTableQuery = createTransitionsTableQuery[:-2] # Remove the trailing ", "
+    createTransitionsTableQuery += (");")
+    cur.execute(createTransitionsTableQuery)
     
     print("Successfully created database " + database_path + ".")
 
@@ -26,31 +28,51 @@ class StateTransitionDatabase:
     def __init__(self, database="data/transition-db/wdb.db"):
         self.__con = sqlite3.connect(database)
         self.__con.execute("PRAGMA foreign_keys = ON;")
+        
+        cursor = self.__get_cursor()
+        cursor.execute("SELECT number_of_parameters FROM metadata;")
+        metadata_row = cursor.fetchone()
+        self.__num_params = metadata_row[0]
+        
+        self.__insert_query = "INSERT INTO transitions VALUES("
+        for i in xrange(self.__num_params*2):
+            self.__insert_query += ("?, ")
+        self.__insert_query = self.__insert_query[:-2] # Remove the trailing ", "
+        self.__insert_query += (");")
     
-    def add_state(self, state):
-        '''
-        @param state: A numpy array to insert into the database. Must be of type float.
-        @return: The ID of the inserted state 
-        '''
-        cur = self.__con.cursor()
-        
-        insertQuery = "INSERT INTO states VALUES(NULL"
-        for i in xrange(len(state)):
-            insertQuery += ", ?"
-        insertQuery += ");"
-        
-        cur.execute(insertQuery, state)
-        self.__con.commit()
-        
-        return cur.lastrowid
+    def __get_cursor(self):
+        return self.__con.cursor()
     
-    def add_transition(self, from_state_id, to_state_id):
-        cur = self.__con.cursor()
-        
-        cur.execute("INSERT INTO transitions(fromState, toState) VALUES(?, ?);", (from_state_id, to_state_id))
+    def add_transition(self, from_state, to_state):
+        '''
+        @param from_state: A numpy array describing a state before transition. Must be of type float.
+        @param from_state: A numpy array describing a state after transition. Must be of type float.
+        '''
+        cur = self.__get_cursor()
+        cur.execute(self.__insert_query, self.combine_transition(from_state, to_state))
         self.__con.commit()
+    
+    def get_transitions(self):
+        """
+        Returns all transitions in the database.
+        """
+        cur = self.__get_cursor()
+        cur.execute("SELECT * FROM transitions;")
         
-        return cur.lastrowid
-
+        return numpy.array(cur.fetchall())
+    
+    def combine_transition(self, from_state, to_state):
+        """
+        Combines the two given states into a transition row.
+        """
+        return numpy.concatenate((from_state, to_state))
+    
+    def split_transition(self, transition):
+        """
+        Splits the given transition row into its two state parts.
+        If transition is a matrix, splits the rows of the matrix and returns the two ("left" and "right") parts of the matrix.
+        """
+        return numpy.hsplit(transition, 2)
+    
 if __name__ == "__main__":
     create_database()
