6f1675504d60dcdb498041a26302d367eaa4c201
Emil Lundberg <emlun@kth.se>
2012-03-26 17:17:33 +0200

    Added generation of falling, bouncing squares, and a tracker for tracking them. Currently works horribly badly.

--- src/wgenerator/square/square_generator.py
+++ src/wgenerator/square/square_generator.py
@@ -133,6 +133,88 @@ def render_online():
     
     print "Done."
 
+def render_bounce(movie_id=0, start_state=None, gravity=numpy.array([0, 9.81]), bounce_factor=0.75, input_to_database=True):
+    """
+    Renders a falling and bouncing square.
+    """
+    
+    dataset = "square_bounce"
+    num_frames = 128
+    dt = 0.1
+    square_side = 50.
+    
+    # State: [x, y, x', y']
+    if start_state is None:
+        delete_database(dataset)
+        render_bounce(0, numpy.array([IMAGE_WIDTH/2, 0, 0., 0.]))
+        render_bounce(1, numpy.array([IMAGE_WIDTH/2, 0, 10., 0.]))
+        render_bounce(2, numpy.array([IMAGE_WIDTH*2/3, IMAGE_HEIGHT/2, -25., -45.]))
+        render_bounce(3, numpy.array([IMAGE_WIDTH/2, IMAGE_HEIGHT-square_side, 0., 0.]))
+        render_bounce(4, numpy.array([IMAGE_WIDTH/2, IMAGE_HEIGHT-square_side, 0., 100]))
+        render_bounce(5, numpy.array([IMAGE_WIDTH/2, IMAGE_HEIGHT-square_side, 20., 50.]))
+        render_bounce(6, numpy.array([IMAGE_WIDTH/2, IMAGE_HEIGHT/2, 300., 0.]))
+        render_bounce(7, numpy.array([0, 0, 50., 0.]))
+        render_bounce(8, numpy.array([IMAGE_WIDTH/2, IMAGE_HEIGHT*3/4, -30., 85.]))
+        render_bounce(9, numpy.array([IMAGE_WIDTH/5, IMAGE_HEIGHT/3, 5., 17.]))
+        return
+    
+    movie_name = dataset + "_" + str(movie_id)
+    save_dir = "data/" + movie_name + ".pngvin"
+    if not os.path.exists(save_dir):
+        os.mkdir(save_dir)
+    
+    print "Rendering pngvin movie", movie_name
+    
+    db = StateTransitionDatabase(dataset, 4)
+    
+    surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, IMAGE_WIDTH, IMAGE_HEIGHT)
+    ctx = cairo.Context(surface)
+    
+    state = start_state
+    print "Initial values:", state
+    
+    for i in xrange(num_frames):
+        next_state = state + numpy.concatenate([state[2:4], gravity])*dt
+        
+        # X collision
+        if next_state[0] <= 0:
+            next_state[2] *= -bounce_factor
+            next_state[0] = 0 - next_state[0]
+        elif next_state[0] >= IMAGE_WIDTH - square_side:
+            next_state[2] *= -bounce_factor
+            next_state[0] = (IMAGE_WIDTH-square_side)  - (next_state[0] - (IMAGE_WIDTH-square_side))
+        
+        # Y collision
+        if next_state[1] >= IMAGE_HEIGHT - square_side:
+            next_state[3] *= -bounce_factor
+            next_state[1] = (IMAGE_HEIGHT-square_side)  - (next_state[1] - (IMAGE_HEIGHT-square_side))
+            
+        if i>0:
+            # Input the transition into the database
+            db.add_transition(state, next_state)
+        
+        state = next_state
+#        print state
+        
+        x = state[0]/IMAGE_WIDTH
+        y = state[1]/IMAGE_HEIGHT
+        
+        clear(ctx)
+
+        ctx.set_source_rgb(0,0,0)
+        ctx.rectangle(0, 0, 1, 1)
+        ctx.fill()
+
+        ctx.set_source_rgb(1, 1, 1)
+        ctx.translate(x, y)
+        ctx.rectangle(0, 0, square_side/IMAGE_WIDTH, square_side/IMAGE_WIDTH)
+        ctx.fill()
+
+        surface.write_to_png(save_dir+"/frame-"+left_align_videoformat(i)+".png")
+
+    print "Completed rendering", movie_name
+
 if (__name__=='__main__'):
-    render_simple()
-    render_online()
+#    render_simple()
+#    render_online()
+    render_bounce()
