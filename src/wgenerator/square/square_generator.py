5df59afb07f30034e104baa64f49195ad0052a12
Emil Lundberg <emlun@kth.se>
2012-04-19 14:28:28 +0200

    Removed render_newton2

--- src/wgenerator/square/square_generator.py
+++ src/wgenerator/square/square_generator.py
@@ -227,100 +227,9 @@ def render_bounce(movie_id=0, start_state=None, gravity=numpy.array([0, 1]), bou
         ctx.set_source_rgb(1, 1, 1)
         ctx.rectangle(x, y, square_side/IMAGE_WIDTH, square_side/IMAGE_WIDTH)
         ctx.fill()
-
+        
         surface.write_to_png(os.path.join(save_dir, "frame-"+left_align_videoformat(i)+".png"))
 
-    print "Completed rendering", movie_name
-
-def render_newton2(movie_id=0, start_state=None, accel_func=lambda a:numpy.zeros_like(a), input_to_database=True):
-    """
-    Renders a falling and bouncing square.
-    """
-    
-    dataset = "square_newton2"
-    num_frames = 128
-    square_side = 50.
-    
-    X_LIMITS = numpy.array((square_side, IMAGE_WIDTH-square_side))*math.sqrt(2)
-    Y_LIMITS = numpy.array((square_side, IMAGE_HEIGHT-square_side))*math.sqrt(2)
-    dt = 0.1
-    
-    # State: [x, y, x', y']
-    if start_state is None:
-        delete_database(dataset)
-        create_database(dataset, [2, 2, 2])
-        # State: [x, x', y, y', theta, theta']
-
-        def gravity_towards_point_and_spring_on_angle(a, gravity_point=numpy.array((IMAGE_WIDTH, IMAGE_HEIGHT))/2.0, G=50, k=0.05):
-            vec = gravity_point - a[0:3:2]
-            return numpy.hstack((G * vec/(numpy.linalg.norm(vec)**2), -k*a[4]))
-        
-        render_newton2(0, numpy.array([IMAGE_WIDTH/3.0, 0., IMAGE_WIDTH/2.0, -5., 1.0, 0.0]), lambda a:gravity_towards_point_and_spring_on_angle(a))
-        render_newton2(1, numpy.array([X_LIMITS[0], 5., Y_LIMITS[0], 0., 0.5, 0.0]), lambda a:gravity_towards_point_and_spring_on_angle(a))
-        return
-    
-    movie_name = dataset + "_" + str(movie_id)
-    save_dir = os.path.join("video", movie_name + ".pngvin")
-    if not os.path.exists(save_dir):
-        os.makedirs(save_dir)
-    
-    print "Rendering pngvin movie", movie_name
-    
-    db = StateTransitionDatabase(dataset)
-    
-    states = numpy.zeros((num_frames,) + start_state.shape)
-    surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, IMAGE_WIDTH, IMAGE_HEIGHT)
-    ctx = cairo.Context(surface)
-    
-    state = start_state
-    
-    print "Initial values:", state
-    
-    def timestep(state, fixpoint_iterations=10):
-        next_state = state
-        a0 = accel_func(state)
-        d0 = numpy.array((state[1], a0[0], state[3], a0[1], state[5], a0[2]))
-        for i in xrange(fixpoint_iterations):
-            a = accel_func(state)
-            d = numpy.array((next_state[1], a[0], next_state[3], a[1], state[5], a[2]))
-            next_state = state + 0.5 * (d0 + d) * dt
-        return next_state
-    
-    for i in xrange(num_frames):
-        
-        if i>0:
-            next_state = timestep(state)
-
-            # Input the transition into the database
-            normalizer = numpy.array((state[0], 0, state[2], 0, state[4], 0))
-            db.add_transitions(state - normalizer, next_state - normalizer)
-        
-            state = next_state
-        
-        x = (state[0]-square_side/2.0)/IMAGE_WIDTH
-        y = (state[2]-square_side/2.0)/IMAGE_HEIGHT
-        theta = state[4]
-        
-        clear(ctx)
-        ctx.rectangle(0, 0, 1, 1)
-        ctx.set_source_rgb(0,0,0)
-        ctx.fill()
-        
-        ctx.set_source_rgb(1, 1, 1)
-        ctx.translate(x, y)
-        ctx.rotate(theta)
-        ctx.rectangle(-square_side/2.0/IMAGE_WIDTH, -square_side/2.0/IMAGE_HEIGHT, square_side/IMAGE_WIDTH, square_side/IMAGE_HEIGHT)
-        ctx.fill()
-        
-#        ctx.set_source_rgb(1, 1, 1)
-#        ctx.rectangle(x, y, square_side/IMAGE_WIDTH, square_side/IMAGE_WIDTH)
-#        ctx.rotate(theta)
-#        ctx.fill()
-
-        states[i] = state
-        surface.write_to_png(os.path.join(save_dir, "frame-"+left_align_videoformat(i)+".png"))
-
-    numpy.save(os.path.join(save_dir, "state_sequence"), states)
     print "Completed rendering", movie_name
 
 def generate_accelerating(number_of_transitions, accel_func=lambda a:numpy.array((0,0,0,1)), debug=False):
@@ -376,5 +285,4 @@ if (__name__=='__main__'):
 #    render_simple()
 #    render_online()
     render_bounce()
-    render_newton2()
     generate_accelerating(1000)
