1cf97429fd37fbc7d8a76b9c5792ec0284e0e260
Emil Lundberg <emlun@kth.se>
2012-04-20 15:57:00 +0200

    Improved the tracker benchmark: Added abstract base class for trackers. Now only the sample and goodness functions need to be implemented, and the ANIMATOR_CLASS attribute needs to be set with an animator class.

--- src/wtracker/tracker.py
+++ src/wtracker/tracker.py
@@ -1,4 +1,48 @@
-__all__ = ["Tracker"]
+import numpy
+from pf import pf
 
 class Tracker:
-    pass
+    ANIMATOR_CLASS = None
+    
+    def __init__(self, db, video):
+        self.db = db
+        self.video = video
+        self.num_frames = len(self.video)
+    
+    def goodness(self, particle, image):
+        raise NotImplementedError("This class is abstract!")
+    
+    def sample(self, prev_particle):
+        raise NotImplementedError("This class is abstract!")
+    
+    def run(self, start_state, num_particles, square_side=50):
+        
+        print "Tracking..."
+        
+        particles = numpy.array([start_state]*num_particles)
+        
+        track = numpy.zeros((self.num_frames, start_state.size))
+        track[0,:] = start_state
+        all_particles = [None]*self.num_frames
+        all_intermediate_particles = [None]*self.num_frames
+        all_particles[0] = particles
+        all_intermediate_particles[0] = particles
+        
+        for i, frame in enumerate(self.video[1:], 1):
+            particles, intermediate_particles = pf(particles, frame.get_array(), self.goodness, sampling_function=self.sample)
+            track[i,:] = particles.mean(axis=0)
+            all_particles[i] = particles
+            all_intermediate_particles[i] = intermediate_particles
+            print "Tracked frame %i of %i"%(i+1, self.num_frames)
+        
+        print "Tracking complete."
+        
+        self.animator = self.ANIMATOR_CLASS(all_particles, square_side, track, all_intermediate_particles)
+        
+        return track
+    
+    def render_results(self, context, frame_i):
+        self.animator.render(context, frame_i)
+    
+    def get_layer(self):
+        return self.animator
