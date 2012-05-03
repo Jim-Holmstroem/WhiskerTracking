12115b215c4aca4c761ea60dbc94a0a51eb5426c
Emil Lundberg <emlun@kth.se>
2012-05-03 11:55:20 +0200

    Added MATLAB script for plotting a spline against a whisker (note: image path is machine specific)

---
+++ matlab/plot_spline.m
@@ -0,0 +1,22 @@
+points = [
+245 84;
+285 90;
+332 88;
+366 80;
+387 71];
+
+x = points(:,1);
+y = points(:,2);
+
+A = [[1;1;1;1;1] x x.^2 x.^3 x.^4];
+a = A\y;
+
+X = 0:640;
+Y = a(1) + a(2)*X + a(3)*X.^2 + a(4)*X.^3 + a(5)*X.^4;
+
+I = imread('/misc/projects/whisker/video/m8_multi-frames/frame-0739.png');
+figure(1);
+clf;
+imshow(I);
+hold on;
+plot(X,Y, 'g--');
