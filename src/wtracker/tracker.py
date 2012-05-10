f2c512c42dea22c8b0c6d64c727436d9063ab215
Emil Lundberg <emlun@kth.se>
2012-05-10 16:08:35 +0200

    Replaced loop over objects in Tracker.run() with map-calls to new method track_object

--- src/wtracker/tracker.py
+++ src/wtracker/tracker.py
@@ -35,35 +35,37 @@ class Tracker:
 
     def get_animator(self, i):
         return self.animators[i]
-    
+
+    def track_object(self, obj_i, start_state):
+        print "Tracking object %i of %i"%(obj_i+1, len(self.start_states))
+        print "Start state for object %i is %s."%(obj_i+1, start_state)
+        
+        self.preresampled_particles[obj_i] = numpy.zeros((self.num_frames, self.num_particles, start_state.size))
+        self.resampled_particles[obj_i] = numpy.zeros((self.num_frames, self.num_particles, start_state.size))
+        
+        particles = numpy.array([start_state]*self.num_particles)
+        
+        track = numpy.zeros((self.num_frames, start_state.size))
+        track[0,:] = start_state
+        self.preresampled_particles[obj_i][0] = particles
+        self.resampled_particles[obj_i][0] = particles
+        
+        for i, frame in enumerate(self.video[1:], 1):
+            particles, intermediate_particles = pf(particles, self.preprocess_image(frame), self.goodness, sampling_function=self.sample)
+            track[i,:] = particles.mean(axis=0)
+            self.resampled_particles[obj_i][i] = particles
+            self.preresampled_particles[obj_i][i] = intermediate_particles
+            print "Tracked frame %i of %i"%(i+1, self.num_frames)
+        
+        print "Tracking complete."
+        print
+        
+        self.tracks[obj_i] = track
+
     def run(self):
         print "%s tracking with %i particles..."%(self.__class__.__name__, self.num_particles)
 
-        for obj_i, start_state in enumerate(self.start_states):
-            print "Tracking object %i of %i"%(obj_i+1, len(self.start_states))
-            print "Start state for object %i is %s."%(obj_i+1, start_state)
-
-            self.preresampled_particles[obj_i] = numpy.zeros((self.num_frames, self.num_particles, start_state.size))
-            self.resampled_particles[obj_i] = numpy.zeros((self.num_frames, self.num_particles, start_state.size))
-
-            particles = numpy.array([start_state]*self.num_particles)
-        
-            track = numpy.zeros((self.num_frames, start_state.size))
-            track[0,:] = start_state
-            self.preresampled_particles[obj_i][0] = particles
-            self.resampled_particles[obj_i][0] = particles
-            
-            for i, frame in enumerate(self.video[1:], 1):
-                particles, intermediate_particles = pf(particles, self.preprocess_image(frame), self.goodness, sampling_function=self.sample)
-                track[i,:] = particles.mean(axis=0)
-                self.resampled_particles[obj_i][i] = particles
-                self.preresampled_particles[obj_i][i] = intermediate_particles
-                print "Tracked frame %i of %i"%(i+1, self.num_frames)
-        
-            print "Tracking complete."
-            print
-
-            self.tracks[obj_i] = track
+        map(self.track_object, xrange(len(self.start_states)), self.start_states)
 
         self.animators = self.make_animators()
     
