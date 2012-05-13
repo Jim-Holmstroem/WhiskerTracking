dcd0b7c8e1ef1adf5549ab0b34c514b32e8b648c
Jim Holmström <jim.holmstroem@gmail.com>
2012-05-13 14:27:33 +0200

    WOrks for 1 frame, make it work for more

--- src/preprocessing_testing/find_relative_motion_snout.m
+++ src/preprocessing_testing/find_relative_motion_snout.m
@@ -17,30 +17,33 @@
 %account later when one tries to transform the whiskersbase
 
 %to fix the issue with image edges when, no support for even-filterse, sry 
+gaussian=gpuArray(fspecial('gaussian',5*3,5));
+prewitt =gpuArray(fspecial('prewitt'));
+
 conv2replicate = @(img,filter) padarray(conv2(img,filter,'valid'),(size(filter)-1)/2,'replicate','both');
 
 edge_response = @(img) sqrt( ...
-                            conv2replicate(img,fspecial('prewitt') ).^2+ ...
-                            conv2replicate(img,fspecial('prewitt')').^2  ...
+                            conv2replicate(img,prewitt ).^2+ ...
+                            conv2replicate(img,prewitt').^2  ...
                            );
 
-search_angle=0%-pi/8:pi/16:(pi/8+pi/16);
+search_angle=0;%-pi/8:pi/16:(pi/8+pi/16);
 search_translation=-25:1:(25+1);
 A=size(search_angle,2);
 T=size(search_translation,2);
 
-search_size=A*T^2;
+%search_size=A*T^2;
 
 start=gpuArray(double(imread('frames/m8_multi-frames/preprocessed_snout/frame-0535.png')));
-image1=gpuArray(double(imread('frames/m8_multi-frames/preprocessed_snout/frame-0536.png')));
+image1=gpuArray(double(imread('frames/m8_multi-frames/preprocessed_snout/frame-0539.png')));
 
-start_edge=conv2replicate(edge_response(start),fspecial('gaussian',5*3,5));
+start_edge=conv2replicate(edge_response(start),gaussian);
 start_edge=start_edge/sum(sum(start_edge));
 
-image1_edge=conv2replicate(edge_response(image1),fspecial('gaussian',5*3,5));
+image1_edge=conv2replicate(edge_response(image1),gaussian);
 image1_edge=image1_edge/sum(sum(image1_edge));
 
-largest_response=[0,0,0,0]; %<dangle,dx,dy,response>
+largest_response=gpuArray([0,0,0,0]); %<dangle,dx,dy,response>
 
 for at=search_angle
     for xt=search_translation
@@ -48,22 +51,22 @@ for at=search_angle
             %image_rot_trans=imtranslate(imrotate(image1_edge,(180/pi)*at,'bilinear','crop'),xt,yt); %rotate then translate
             image_rot_trans=imtranslate(image1_edge,xt,yt); %rotate then translate
             image_rot_trans=image_rot_trans/sum(sum(image_rot_trans));
-            response=sum(sum(image_rot_trans.*start_edge)); %correlate the edges
+            response=(sum(sum(image_rot_trans.*start_edge))); %correlate the edges
             
             if(largest_response(4)<response)
-                largest_response=[at,xt,yt,response];
+                largest_response=[at,-xt,-yt,response];
             end
-            search_size=search_size-1;
-            if(mod(search_size,100)==0)
-                disp(search_size)
-            end
+ %           search_size=search_size-1;
+ %           if(mod(search_size,100)==0)
+ %               disp(search_size)
+ %           end
         end
     end
 end
-largest_response
+
+gather(largest_response)
 
 %THE RESULT
 %start_rot_trans=imtranslate(imrotate(start_edge,(180/pi)*largest_response(1),'bilinear','crop'),largest_response(2),largest_response(3)); %rotate then translate
 %imagesc(start_rot_trans-image1_edge)
-
 
