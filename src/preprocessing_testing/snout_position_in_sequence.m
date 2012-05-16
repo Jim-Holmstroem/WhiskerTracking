8f1926b35e47f2b978bc640dc00491342e5a4260
Jim Holmström <jim.holmstroem@gmail.com>
2012-05-16 10:49:48 +0200

    the snoutfinder worksgit status

--- src/preprocessing_testing/snout_position_in_sequence.m
+++ src/preprocessing_testing/snout_position_in_sequence.m
@@ -8,39 +8,45 @@ N=size(imgs,3);%number of frames
 
 positions=zeros(N,4);
 
-gaussian=(fspecial('gaussian',5*3,5));
+sigma=5;
+gaussian=(fspecial('gaussian',3*sigma,sigma));
 
 search_angle=0;%-pi/8:pi/16:(pi/8+pi/16);NOTE currently no support for angles!
 assert(search_angle==0);
-search_translation=-20:1:(20+1);
+search_translation=-5:1:5;
 A=size(search_angle,2);
 T=size(search_translation,2);
 
 search_size=A*T^2;
 
 ref_edge=conv2replicate(edge_response(ref),gaussian);
-ref_edge=start_edge/sum(sum(ref_edge));
+ref_edge=ref_edge/max(max(ref_edge));
 
-last.a=0;
+last.a=0; %relative to ref
 last.x=0;
 last.y=0;
 
-for frame=N
-    img=sequence_frames(:,:,frame);
+for frame=1:N
+    img=imgs(:,:,frame);
     img_edge=conv2replicate(edge_response(img),gaussian);
-    img_edge=img_edge/sum(sum(img_edge));
+    img_edge=img_edge/max(max(img_edge));
     largest_response=([0,0,0,0]); %<dangle,dx,dy,response>
     for at=search_angle
         for xt=search_translation
             for yt=search_translation
-                ref_try=imtranslate(ref_edge,last.x+xt,last.y+yt);  
-                response=(sum(sum(img.*ref_try))); %correlate the edges
+                ref_try=imtranslate(ref_edge,last.x+xt,last.y+yt);
+                mask=img_edge.*ref_try;
+                response=(sum(sum(mask))); %correlate the edges
                 if(largest_response(4)<response)
                     largest_response=[last.a+at,last.x+xt,last.y+yt,response];
                 end
             end
         end
     end
+
+    imshow(img_edge/2+imtranslate(ref_edge,largest_response(2),largest_response(3))/2);
+    drawnow;
+
     positions(frame,:)=gather(largest_response)';
     last.a=largest_response(1);
     last.x=largest_response(2);
--- src/preprocessing_testing/test_snout_positions_in_sequence.m
+++ src/preprocessing_testing/test_snout_positions_in_sequence.m
@@ -4,11 +4,25 @@ ref=(double(imread(strcat('frames/m8_multi-frames/preprocessed_snout/frame-0',in
 
 imgs=zeros([size(ref) size(test_range,1)]);
 
-for frame=test_range
-    imgs(:,:,frame)=(double(imread(strcat('frames/m8_multi-frames/preprocessed_snout/frame-0',int2str(test_range(frame)),'.png'))));
+for idx=1:size(test_range,2)
+    imgs(:,:,idx)=(double(imread(strcat('frames/m8_multi-frames/preprocessed_snout/frame-0',int2str(test_range(idx)),'.png'))));
 end
+
+size(imgs)
 
 disp('loading done');
 positions = snout_position_in_sequence(ref,imgs);
+disp('preprocessing done');
 
-
+while (1)
+for idx=1:size(test_range,2)
+    hold off;
+    imshow(imgs(:,:,idx));
+    hold on;
+    warning('ref_centroid hardcoded')
+    plot(260+positions(idx,2),85+positions(idx,3),'rx');
+    title(strcat('response=',int2str(positions(idx,4))));
+    drawnow;
+    pause;
+end
+end
