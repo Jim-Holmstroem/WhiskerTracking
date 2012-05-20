e2653f954662cf8532053d43dccae0dbfb6d0e71
Emil Lundberg <emlun@kth.se>
2012-05-20 23:49:19 +0200

    Improved plotting script

--- src/process-benchmark.py
+++ src/process-benchmark.py
@@ -22,7 +22,6 @@ ss = (0.25, 0.5, 1, 2, 4)
 tensor_dim = (len(err_norms), len(nn), len(pp), len(aa), len(gg), len(ss), num_whiskers)
 
 E = numpy.ones(tensor_dim) * 1E18
-worst = numpy.ones(tensor_dim[:-1]) * 1E18
 
 for ni, n in enumerate(nn):
     for pi, p in enumerate(pp):
@@ -42,20 +41,18 @@ for ni, n in enumerate(nn):
 #                    print absolute_rms_time.max(axis=0)
                     for i, norm in enumerate(err_norms):
                         E[i,ni,pi,ai,gi,si,:] = absolute_rms_time[:,i]
-                        worst[i,ni,pi,ai,gi,si] = E[i,ni,pi,ai,gi,si,:].max()
 
 def get_params_for_index(index):
     return (nn[index[0]], pp[index[1]], aa[index[2]], gg[index[3]], ss[index[4]])
 
 for i, P in enumerate(err_norms):
-    worst = E[i].max(axis=5)
-    print worst.min()
+    maxerr = E[i].max(axis=5)
     
+    best_maxerr = numpy.where(maxerr == maxerr.min())
     best = numpy.where(E[i] == E[i].min())
-    best_worst = numpy.where(worst == worst.min())
 
-    print "Least error for L%i: %f\tn=%i, p=%i, a=%i, g=%i, s=%i"%((P, E[i].min()) + get_params_for_index(best))
-    print "Least maximum error for L%i: %f\tn=%i, p=%i, a=%i, g=%i, s=%i"%((P, E[i].min()) + get_params_for_index(best_worst))
+    print "Least maxerr for L%i: %f\tn=%i, p=%i, a=%i, g=%i, s=%i"%((P, maxerr.min()) + get_params_for_index(best_maxerr))
+    print "Least error for L%i:  %f\tn=%i, p=%i, a=%i, g=%i, s=%i"%((P, E[i].min()) + get_params_for_index(best))
 
     fig = plt.figure()
     ax = Axes3D(fig)
@@ -63,15 +60,15 @@ for i, P in enumerate(err_norms):
     Y = pp
     
     X, Y = numpy.meshgrid(X, Y)
-    Z = E[i,1,:,1:,1,3,1]
+    Z = maxerr[1,:,1:,1,3]
     Z[numpy.where(Z > 1E17)] = None
     
     surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.jet)
-    ax.set_zlim3d(0, 150)
-    ax.w_zaxis.set_major_locator(LinearLocator(10))
-    ax.w_zaxis.set_major_formatter(FormatStrFormatter('%.03f'))
+    #ax.set_zlim3d(0, 150)
+    #ax.w_zaxis.set_major_locator(LinearLocator(10))
+    #ax.w_zaxis.set_major_formatter(FormatStrFormatter('%.03f'))
 
-    ax.set_title("")
+    ax.set_title("Maxerr vs. a and p in L%i"%(P))
     ax.set_xlabel('a')
     ax.set_ylabel('p')
     ax.set_zlabel('E')
